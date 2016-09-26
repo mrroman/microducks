e745510775c52234b04d8e52eb6cc72eaef6e96e
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-09-26 22:52:09 +0300

    Add Object.assign to Utils

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -13,6 +13,28 @@ const Utils = {
         };
     },
 
+    // copyied from https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+    assign(target) {
+        'use strict';
+        // We must check against these specific cases.
+        if (target === undefined || target === null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var output = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source !== undefined && source !== null) {
+                for (var nextKey in source) {
+                    if (source.hasOwnProperty(nextKey)) {
+                        output[nextKey] = source[nextKey];
+                    }
+                }
+            }
+        }
+        return output;
+    },
+
     merge(previous, next, setFunc, clearFunc) {
         let toClear = Object.keys(previous).filter((name) => {
                 return (name in previous) && !(name in next);
